530f3166d3ca1a2b514b3f1b49a178505e39edc5
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-06-19 19:15:11 +0530

    modified:   admin/search-student-data.php

--- admin/search-student-data.php
+++ admin/search-student-data.php
@@ -30,46 +30,65 @@
             <tbody>
                 <?php
                 require('includes/connection.php');
+                if (isset($_POST['delete'])) {
+                    $student_id = $_POST['student_id'];
 
-                $results_per_page = 10;
-                $fetch_students = "SELECT * FROM `bora_student` ORDER BY student_added_date DESC";
-                $fetch_res = mysqli_query($connection, $fetch_students);
-                $count = mysqli_num_rows($fetch_res);
+                    $delete_query = "DELETE FROM `bora_student` WHERE `student_id` = '$student_id'";
+                    $delete_res = mysqli_query($connection, $delete_query);
 
-                $number_of_page = ceil($count / $results_per_page);
-
-                if (!isset($_GET['page'])) {
-                    $page = 1;
-                } else {
-                    $page = $_GET['page'];
+                    if ($delete_res) { ?>
+                <div class="w-100 alert alert-success mt-3 mb-3" role="alert">Student Deleted!</div>
+                <?php
+                    }
                 }
 
-                $page_first_result = ($page - 1) * $results_per_page;
-                $page_query = "SELECT * FROM `bora_student` LIMIT "  . $page_first_result . ',' . $results_per_page;
-                $page_result = mysqli_query($connection, $page_query);
+                if (isset($_POST['search'])) {
 
-                while ($row = mysqli_fetch_assoc($page_result)) {
-                    $student_id = $row['student_id'];
-                    $student_img = "assets/student/" . $row['student_img'];
-                    $student_name = $row['student_name'];
-                    $student_contact = $row['student_contact'];
-                    $student_course = $row['student_course'];
-                    $student_roll = $row['student_roll'];
-                    $student_admission_year = $row['student_admission_year'];
-                    $student_added_by = $row['student_added_by']; ?>
+                    $student_search = $_POST['student_search'];
+                    $results_per_page = 10;
+                    $fetch_students = "SELECT * FROM `bora_student` ORDER BY student_added_date DESC";
+                    $fetch_res = mysqli_query($connection, $fetch_students);
+                    $count = mysqli_num_rows($fetch_res);
+
+                    $number_of_page = ceil($count / $results_per_page);
+
+                    if (!isset($_GET['page'])) {
+                        $page = 1;
+                    } else {
+                        $page = $_GET['page'];
+                    }
+
+                    $page_first_result = ($page - 1) * $results_per_page;
+                    $page_query = "SELECT * FROM `bora_student` WHERE 
+                `student_name` LIKE '%$student_search%' OR 
+                `student_contact` LIKE '%$student_search%' OR 
+                `student_roll` LIKE '%$student_search%' OR 
+                `student_course` LIKE '%$student_search%' OR 
+                `student_aadhar_number` LIKE '%$student_search%' LIMIT "  . $page_first_result . ',' . $results_per_page;
+                    $page_result = mysqli_query($connection, $page_query);
+
+                    while ($row = mysqli_fetch_assoc($page_result)) {
+                        $student_id = $row['student_id'];
+                        $student_img = "assets/student/" . $row['student_img'];
+                        $student_name = $row['student_name'];
+                        $student_contact = $row['student_contact'];
+                        $student_course = $row['student_course'];
+                        $student_roll = $row['student_roll'];
+                        $student_admission_year = $row['student_admission_year'];
+                        $student_added_by = $row['student_added_by']; ?>
                 <tr>
 
                     <td><?php echo $student_roll ?></td>
                     <th scope="row"><?php echo $student_name ?></th>
                     <td><?php echo $student_contact ?></td>
                     <td><?php
-                            $fetch_course_name = "SELECT * FROM `bora_course` WHERE `course_id` = '$student_course'";
-                            $fetch_course_name_res = mysqli_query($connection, $fetch_course_name);
-                            $course_name = "";
-                            while ($row = mysqli_fetch_assoc($fetch_course_name_res)) {
-                                $course_name = $row['course_name'];
-                            }
-                            echo $course_name ?></td>
+                                $fetch_course_name = "SELECT * FROM `bora_course` WHERE `course_id` = '$student_course'";
+                                $fetch_course_name_res = mysqli_query($connection, $fetch_course_name);
+                                $course_name = "";
+                                while ($row = mysqli_fetch_assoc($fetch_course_name_res)) {
+                                    $course_name = $row['course_name'];
+                                }
+                                echo $course_name ?></td>
                     <td><?php echo $student_admission_year ?></td>
                     <td>
                         <form action="student-details.php" method="post">
@@ -78,29 +97,19 @@
                                 Details</button>
                         </form>
                     </td>
-                    <!-- <td>
-                            <form action="collect-fee.php" method="POST">
-                                <input type="text" value="<?php echo $student_id ?>" name="student_id" hidden>
-                                <button type="submit" name="collect" class="btn btn-sm btn-outline-warning">Collect
-                                    Fee</button>
-                            </form>
-                        </td> -->
+                    <td>
+                        <form action="" method="POST">
+                            <input type="text" value="<?php echo $student_id ?>" name="student_id" hidden>
+                            <button type="submit" name="delete" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+                    </td>
                 </tr>
                 <?php
+                    }
                 }
                 ?>
             </tbody>
         </table>
-
-        <!-- <nav aria-label="Page navigation example" class="w-100 mt-3">
-            <ul class="pagination">
-                <?php
-                for ($page = 1; $page <= $number_of_page; $page++) {
-                    echo '<li class="page-item"><a class="page-link" href="view-students.php?page=' . $page . '">' . $page . ' </a></li>';
-                }
-                ?>
-            </ul>
-        </nav> -->
     </div>
 </div>
 <?php include('includes/footer.php') ?>
