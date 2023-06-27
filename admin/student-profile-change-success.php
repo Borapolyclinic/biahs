0ddc90eb399efc634840490625839730ed250582
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-06-27 09:47:12 +0530

    new file:   admin/admin-change-student-profile.php modified:   admin/assets/styles.css modified:   admin/student-details.php new file:   admin/student-profile-change-success.php

---
+++ admin/student-profile-change-success.php
@@ -0,0 +1,32 @@
+<?php require('includes/header.php') ?>
+<div class="container mt-5 add-user-success">
+    <?php
+    require('includes/connection.php');
+    if (isset($_POST['front'])) {
+        $student_id = $_POST['student_id'];
+        $student_img = $_FILES["student_img"]["name"];
+        $tempname = $_FILES["student_img"]["tmp_name"];
+        $folder = "assets/student/" . $student_img;
+        $update_query = "UPDATE
+            `bora_student`
+        SET
+            `student_img` = '$student_img'
+           WHERE
+            `student_id` = $student_id";
+        $update_res = mysqli_query($connection, $update_query);
+
+        if ($update_res) {
+            move_uploaded_file($tempname, $folder); ?>
+    <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_lk80fpsm.json" background="transparent" speed="1"
+        style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+    <p>Success! Profile Image Updated.</p>
+    <form action="view-students.php" method="POST" class="w-100 d-flex justify-content-center align-items-center">
+        <input type="text" name="student_img" value="<?php echo $student_img ?>" hidden>
+        <button type="submit" name="edit" class="btn btn-sm btn-outline-success">Go Back</button>
+    </form>
+    <?php
+        }
+    }
+    ?>
+</div>
+<?php require('includes/footer.php') ?>
