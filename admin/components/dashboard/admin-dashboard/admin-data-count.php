8ea32ff2e42d74a9df338f0705a9ab761c541c72
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-06-30 14:05:22 +0530

    modified:   admin/components/dashboard/admin-dashboard/admin-data-count.php

--- admin/components/dashboard/admin-dashboard/admin-data-count.php
+++ admin/components/dashboard/admin-dashboard/admin-data-count.php
@@ -12,12 +12,11 @@
     $user_res = mysqli_query($connection, $user_query);
     $count = mysqli_num_rows($user_res);
     ?>
-
     <div class="w-100 mb-3">
         <form action="search-student-data.php" method="POST" class="filter-row w-100 dashboard-tab p-3">
             <div class="w-100 m-1">
-                <select name="search_type" class="form-select" aria-label="Default select example">
-                    <option selected>Click here for options</option>
+                <select name="search_type" class="form-select" aria-label="Default select example" required>
+                    <option value="null">Click here for options</option>
                     <option value="1">Name</option>
                     <option value="2">Mobile Number</option>
                     <option value="3">UID</option>
@@ -25,7 +24,7 @@
             </div>
             <div class="w-100 m-1">
                 <input type="text" name="student_search" class="form-control filter-input-box"
-                    id="exampleFormControlInput1" placeholder="Enter Name, Mobile Number, UID to search user" required>
+                    id="exampleFormControlInput1" placeholder="" required>
             </div>
             <button type="submit" name="search" class="btn btn-outline-success">Search</button>
         </form>
@@ -49,11 +48,10 @@
         <form action="view-students-by-course.php" method="POST" class="dashboard-tab p-4">
             <div class="filter-row">
                 <select name="course_id" class="form-select m-1" aria-label="Default select example">
-                    <option selected>Select Course</option>
+                    <option value="null">Select Course</option>
                     <?php
                     $fetch_course = "SELECT * FROM `bora_course`";
                     $fetch_course_res = mysqli_query($connection, $fetch_course);
-
                     while ($row = mysqli_fetch_assoc($fetch_course_res)) {
                         $course_id = $row['course_id'];
                         $course_name = $row['course_name'];
@@ -64,21 +62,8 @@
 
                 <div class="w-100 m-1">
                     <input type="number" name="course_year" placeholder="Enter Year" min="1999" max="2025"
-                        class="form-control filter-input-box w-100">
+                        class="form-control filter-input-box w-100" required>
                 </div>
-
-                <!-- <select class="form-select m-1" aria-label="Default select example" required>
-                    <option selected>Select Year</option>
-                    <?php
-                    $fetch_course_year = "SELECT * FROM `bora_student`";
-                    $fetch_course_year_res = mysqli_query($connection, $fetch_course_year);
-                    $student_admission_year = "";
-                    while ($row = mysqli_fetch_assoc($fetch_course_year_res)) {
-                        $student_admission_year = $row['student_admission_year'];
-                    ?>
-                    <option value="<?php echo $student_admission_year ?>"><?php echo $student_admission_year ?></option>
-                    <?php } ?>
-                </select> -->
                 <button type="submit" name="filter-course" class="ml-2 btn w-100 btn-success">Filter</button>
             </div>
         </form>
