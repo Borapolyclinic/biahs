3446f3eaa1fb48b2fec15acf0161ccfd984d8e3d
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-07-05 14:48:51 +0530

    modified:   admin/includes/header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -2,9 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta name="robots" content="noindex" />
+    <meta name="robots" content="noindex, nofollow">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
 
     <!-- =========== SEO =========== -->
     <meta name="keywords" content="">
