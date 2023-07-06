ba4e629d3ad693913e4d7ca9016295bcffdd29a2
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-07-06 10:34:14 +0530

    modified:   admin/upload-grad-marksheet.php

--- admin/upload-grad-marksheet.php
+++ admin/upload-grad-marksheet.php
@@ -10,10 +10,10 @@
         $folder = "assets/grad_marksheet/" . $student_graduation_marksheet;
 
         if ($student_status == '1' && empty($student_graduation_marksheet)) { ?>
-            <div class="alert alert-danger" role="alert">
-                Error! No file selected to upload.
-            </div>
-            <?php } else {
+    <div class="alert alert-danger" role="alert">
+        Error! No file selected to upload.
+    </div>
+    <?php } else {
             $update_query = "UPDATE
             `bora_student`
         SET
@@ -25,12 +25,13 @@
 
             if ($update_res) {
                 move_uploaded_file($tempname, $folder); ?>
-                <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_lk80fpsm.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
-                <p>Success! Graduation Marksheet Uploaded.</p>
-                <form action="student-details.php" method="POST" class="w-100 d-flex justify-content-center align-items-center">
-                    <input type="text" name="student_id" value="<?php echo $student_id ?>" hidden>
-                    <button type="submit" name="edit_back" class="btn btn-sm btn-outline-success">Go Back</button>
-                </form>
+    <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_lk80fpsm.json" background="transparent" speed="1"
+        style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+    <p>Success! Student status updated.</p>
+    <form action="student-details.php" method="POST" class="w-100 d-flex justify-content-center align-items-center">
+        <input type="text" name="student_id" value="<?php echo $student_id ?>" hidden>
+        <button type="submit" name="edit_back" class="btn btn-sm btn-outline-success">Go Back</button>
+    </form>
     <?php
             }
         }
