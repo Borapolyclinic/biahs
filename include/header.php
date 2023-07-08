7804351540ec6e18e8d1469c9dce9bad8eb649bf
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-07-08 12:38:35 +0530

    modified:   include/header.php

--- include/header.php
+++ include/header.php
@@ -34,4 +34,17 @@
         rel="stylesheet">
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FELH6HTTD8"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-FELH6HTTD8');
+</script>
+
 <body>
